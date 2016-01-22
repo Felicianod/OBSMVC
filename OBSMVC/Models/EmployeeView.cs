1e60d5cd3282a19544349e9ab8fdae4f54114422
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-01-22 10:36:48 -0600

    .

---
+++ OBSMVC/Models/EmployeeView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OBSMVC.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class EMPLOYEE
+    {
+
+    }
+    public class EmployeeMetaData
+    {
+        [Display(Name = "Employee Id")]
+        public int dsc_emp_id { get; set; }
+        [Display(Name = "Employee LC")]
+        public int? dsc_assigned_lc_id { get; set; }
+        [Display(Name = "JDE Id")]
+        public string dsc_emp_perm_id { get; set; }
+        [Display(Name = "Clock Nbr")]
+        public int? dsc_emp_wms_clock_nbr { get; set; }
+        [Display(Name = "First Name")]
+        public string dsc_emp_first_name { get; set; }
+        [Display(Name = "Last Name")]
+        public string dsc_emp_last_name { get; set; }
+        [Display(Name = "Email Address")]
+        public string dsc_emp_email_addr { get; set; }
+        [Display(Name = "Title")]
+        public string dsc_emp_title { get; set; }
+        [Display(Name = "ADP Id")]
+        public string dsc_emp_adp_id { get; set; }
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "MMM dd, yyyy")]
+        public DateTime? dsc_emp_hire_dt { get; set; }
+        [Display(Name = "Work Start Date")]
+        public DateTime dsc_emp_init_work_dt { get; set; }
+        [Display(Name = "Termination Date")]
+        public string dsc_emp_term_dt { get; set; }
+        [Display(Name = "Observable")]
+        public string dsc_emp_can_be_obs_yn { get; set; }
+        [Display(Name = "Temp")]
+        public string dsc_emp_temp_yn { get; set; }
+        [Display(Name = "Hourly")]
+        public string dsc_emp_hourly_yn { get; set; }
+        [Display(Name = "Emp Added By")]
+        public DateTime dsc_emp_added_id { get; set; }
+        [Display(Name = "Emp Added Date-Time")]
+        public string dsc_emp_added_dtm { get; set; }
+        [Display(Name = "Emp Updated By")]
+        public DateTime dsc_emp_upd_uid { get; set; }
+        [Display(Name = "Emp Updated Date-Time")]
+        public DateTime? dsc_emp_upd_dtm { get; set; }
+    }
+}

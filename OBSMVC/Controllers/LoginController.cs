b5da048170796d1f53da043ac7a75b354c73c7ab
dsclogistics <feliciano.delgado@dsc-logistics.com>
2016-01-24 02:59:57 -0600

    .

--- OBSMVC/Controllers/LoginController.cs
+++ OBSMVC/Controllers/LoginController.cs
@@ -28,59 +28,59 @@ namespace OBSMVC.Controllers
             request.Method = "POST";
             request.ContentType = "application/json";
             string parsedContent = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
-                ASCIIEncoding encoding = new ASCIIEncoding();
-                Byte[] bytes = encoding.GetBytes(parsedContent);
-                Stream newStream = request.GetRequestStream();
-                newStream.Write(bytes, 0, bytes.Length);
-                newStream.Close();
-                string JsonString;
-                string errorJsonString;
-                try
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] bytes = encoding.GetBytes(parsedContent);
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(bytes, 0, bytes.Length);
+            newStream.Close();
+            string JsonString;
+            string errorJsonString;
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
                 {
-                    WebResponse response = request.GetResponse();
-                    using (Stream responseStream = response.GetResponseStream())
-                    {
-                        StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
-                        JsonString = reader.ReadToEnd();
-                    }//end of using
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    JsonString = reader.ReadToEnd();
+                }//end of using
 
-                    JavaScriptSerializer ScriptSerializer = new JavaScriptSerializer();
-                    dynamic JsonObject = ScriptSerializer.Deserialize<Dictionary<string, string>>(JsonString);
-                    //use JsonObject to retrieve json data
-                    Session.Add("session_token", JsonObject["session_token"]);
-                    Session.Add("session_id", JsonObject["session_id"]);
-                    Session.Add("first_name", JsonObject["first_name"]);
-                    Session.Add("last_name", JsonObject["last_name"]);
-                    Session.Add("username", username);
-                    Session.Add("email", JsonObject["email"]);
-                    FormsAuthentication.SetAuthCookie(username, true);
-                    if (Url.IsLocalUrl(ReturnUrl) && ReturnUrl.Length > 1 && ReturnUrl.StartsWith("/")
-                        && !ReturnUrl.StartsWith("//") && !ReturnUrl.StartsWith("/\\"))
-                    {
-                        return Redirect(ReturnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                    //return RedirectToAction("Index", "Home");
-                }//end of try
-                catch (WebException ex)
+                JavaScriptSerializer ScriptSerializer = new JavaScriptSerializer();
+                dynamic JsonObject = ScriptSerializer.Deserialize<Dictionary<string, string>>(JsonString);
+                //use JsonObject to retrieve json data
+                Session.Add("session_token", JsonObject["session_token"]);
+                Session.Add("session_id", JsonObject["session_id"]);
+                Session.Add("first_name", JsonObject["first_name"]);
+                Session.Add("last_name", JsonObject["last_name"]);
+                Session.Add("username", username);
+                Session.Add("email", JsonObject["email"]);
+                FormsAuthentication.SetAuthCookie(username, true);
+                if (Url.IsLocalUrl(ReturnUrl) && ReturnUrl.Length > 1 && ReturnUrl.StartsWith("/")
+                    && !ReturnUrl.StartsWith("//") && !ReturnUrl.StartsWith("/\\"))
                 {
-                    WebResponse errorResponse = ex.Response;
-                    using (Stream responseStream = errorResponse.GetResponseStream())
-                    {
-                        StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
-                        errorJsonString = reader.ReadToEnd();
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                //return RedirectToAction("Index", "Home");
+            }//end of try
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    errorJsonString = reader.ReadToEnd();
 
-                    }//end of using
+                }//end of using
 
-                    JavaScriptSerializer ScriptSerializer = new JavaScriptSerializer();
-                    dynamic JsonObject = ScriptSerializer.Deserialize<Dictionary<string, dynamic>>(errorJsonString);
-                    //errorLabel.Text = JsonObject["error"]["message"];
-                    ViewBag.errorMessage = JsonObject["error"]["message"];
-                    return View();
-                }//end of catch
+                JavaScriptSerializer ScriptSerializer = new JavaScriptSerializer();
+                dynamic JsonObject = ScriptSerializer.Deserialize<Dictionary<string, dynamic>>(errorJsonString);
+                //errorLabel.Text = JsonObject["error"]["message"];
+                ViewBag.errorMessage = JsonObject["error"]["message"];
+                return View();
+            }//end of catch
         }//end of OBSLogin
 
         public ActionResult OBSLogout()

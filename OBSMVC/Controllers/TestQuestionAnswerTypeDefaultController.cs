793f22751c25b36ee39cf34665f1e150cfebee5e
Rasul Abduguev <rasul.abduguev@dsc-logistics.com>
2016-02-08 16:09:49 -0600

    2/8 4:09

--- OBSMVC/Controllers/TestQuestionAnswerTypeDefaultController.cs
+++ OBSMVC/Controllers/TestQuestionAnswerTypeDefaultController.cs
@@ -65,98 +65,101 @@ namespace OBSMVC.Controllers
             string answer_type_id = formData["list_of_answers"];
             string question_id = formData["question_id"];
             string isSave = formData["save"];
-
-            if (String.IsNullOrEmpty(answer_type_id))
+            if (isSave == "false")
             {
-                foreach (var x in all_answer_types)
+                if (String.IsNullOrEmpty(answer_type_id))
                 {
-                    SelectListItem answer_type = new SelectListItem();
-                    answer_type.Value = x.obs_ans_type_id.ToString();
-                    answer_type.Text = x.obs_ans_type_name;
-                    answer_type.Selected = false;
-                    list_of_answers.Add(answer_type);                   
-                }
-                ViewBag.list_of_answers = list_of_answers;
-                ViewBag.question_selected_ans_type = question_selected_ans_type;
-                return View(list_of_answers);
-            }
-            else
-            {
-                int selected_ans_type_id = Convert.ToInt32(answer_type_id);
-                int quest_id = Convert.ToInt32(question_id);
-                int selected_obs_qat_id = -1;
-                var obs_qat_id = (from qat in db.OBS_QUEST_ANS_TYPES.Where(x => x.obs_question_id == quest_id && x.obs_ans_type_id== selected_ans_type_id)
-                                           select new { qat.obs_qat_id }).ToList();
-                try
-                {
-                    selected_obs_qat_id = obs_qat_id.FirstOrDefault().obs_qat_id;
-                   
-                }
-                catch (NullReferenceException)
-                { }
-                foreach (var x in all_answer_types)
-                {
-                    SelectListItem answer_type = new SelectListItem();
-                    answer_type.Value = x.obs_ans_type_id.ToString();
-                    answer_type.Text = x.obs_ans_type_name;
-                    if (x.obs_ans_type_id == selected_ans_type_id)
+                    foreach (var x in all_answer_types)
                     {
-                        answer_type.Selected = true;
-                        if (x.obs_ans_type_has_fxd_ans_yn == "Y"&& selected_obs_qat_id!=-1)
-                        {
-                            question_selected_ans_type = db.OBS_QUEST_SLCT_ANS.Where(item => item.obs_qat_id == selected_obs_qat_id).ToList();
-                            ViewBag.question_selected_ans_type = question_selected_ans_type;
-                        }
-                        else if(x.obs_ans_type_has_fxd_ans_yn == "Y")
-                        {
-                            List<string> q_selected_ans_type = new List<string>();
-                             switch (x.obs_ans_type_category)
-                             {
-
-                                 case "3 Val Range":                           
-                                      q_selected_ans_type.Add("Bad");
-                                      q_selected_ans_type.Add("OK");
-                                      q_selected_ans_type.Add("Good");
-                                 break;
-                                 case "5 Val Range":
-                                     q_selected_ans_type.Add("Monday");
-                                     q_selected_ans_type.Add("Tuesday");
-                                     q_selected_ans_type.Add("Wednesday");
-                                     q_selected_ans_type.Add("Thursday");
-                                     q_selected_ans_type.Add("Friday");
-                                  break;
-                                 case "MS List":
-                                     q_selected_ans_type.Add("MS List Item 1");
-                                     q_selected_ans_type.Add("MS List Item 2");
-                                     q_selected_ans_type.Add("MS List Item 3");
-                                     q_selected_ans_type.Add("MS List Item 4");
-                                     q_selected_ans_type.Add("MS List Item 5");
-                                  break;
-                                 case "SS List":
-                                    q_selected_ans_type.Add("Single Selected List Item 1");
-                                    q_selected_ans_type.Add("Single Selected List Item 2");
-                                    q_selected_ans_type.Add("Single Selected List Item 3");
-                                    q_selected_ans_type.Add("Single Selected List Item 4");
-                                    break;
-
-                             }//end of switch
-                            
-                            //QuestionHelpers.getDefault_Selected_Ans_list(x.obs_ans_type_category);
-                            ViewBag.q_selected_ans_type = q_selected_ans_type;
-                        }//end of else if(x.obs_ans_type_has_fxd_ans_yn == "Y")
-
-                    }//end of if (x.obs_ans_type_id == selected_ans_type_id)
-                    else
-                    {
+                        SelectListItem answer_type = new SelectListItem();
+                        answer_type.Value = x.obs_ans_type_id.ToString();
+                        answer_type.Text = x.obs_ans_type_name;
                         answer_type.Selected = false;
+                        list_of_answers.Add(answer_type);
                     }
-                    list_of_answers.Add(answer_type);
-                }//end of foreach
+                    ViewBag.list_of_answers = list_of_answers;
+                    ViewBag.question_selected_ans_type = question_selected_ans_type;
+                    return View(list_of_answers);
+                }
+                else
+                {
+                    int selected_ans_type_id = Convert.ToInt32(answer_type_id);
+                    int quest_id = Convert.ToInt32(question_id);
+                    int selected_obs_qat_id = -1;
+                    var obs_qat_id = (from qat in db.OBS_QUEST_ANS_TYPES.Where(x => x.obs_question_id == quest_id && x.obs_ans_type_id == selected_ans_type_id)
+                                      select new { qat.obs_qat_id }).ToList();
+                    try
+                    {
+                        selected_obs_qat_id = obs_qat_id.FirstOrDefault().obs_qat_id;
 
-                ViewBag.list_of_answers = list_of_answers;
-                return View(list_of_answers);
-            }
-                    
+                    }
+                    catch (NullReferenceException)
+                    { }
+                    foreach (var x in all_answer_types)
+                    {
+                        SelectListItem answer_type = new SelectListItem();
+                        answer_type.Value = x.obs_ans_type_id.ToString();
+                        answer_type.Text = x.obs_ans_type_name;
+                        if (x.obs_ans_type_id == selected_ans_type_id)
+                        {
+                            answer_type.Selected = true;
+                            if (x.obs_ans_type_has_fxd_ans_yn == "Y" && selected_obs_qat_id != -1)
+                            {
+                                question_selected_ans_type = db.OBS_QUEST_SLCT_ANS.Where(item => item.obs_qat_id == selected_obs_qat_id).ToList();
+                                ViewBag.question_selected_ans_type = question_selected_ans_type;
+                            }
+                            else if (x.obs_ans_type_has_fxd_ans_yn == "Y")
+                            {
+                                List<string> q_selected_ans_type = new List<string>();
+                                switch (x.obs_ans_type_category)
+                                {
+
+                                    case "3 Val Range":
+                                        q_selected_ans_type.Add("Bad");
+                                        q_selected_ans_type.Add("OK");
+                                        q_selected_ans_type.Add("Good");
+                                        break;
+                                    case "5 Val Range":
+                                        q_selected_ans_type.Add("Monday");
+                                        q_selected_ans_type.Add("Tuesday");
+                                        q_selected_ans_type.Add("Wednesday");
+                                        q_selected_ans_type.Add("Thursday");
+                                        q_selected_ans_type.Add("Friday");
+                                        break;
+                                    case "MS List":
+                                        q_selected_ans_type.Add("MS List Item 1");
+                                        q_selected_ans_type.Add("MS List Item 2");
+                                        q_selected_ans_type.Add("MS List Item 3");
+                                        q_selected_ans_type.Add("MS List Item 4");
+                                        q_selected_ans_type.Add("MS List Item 5");
+                                        break;
+                                    case "SS List":
+                                        q_selected_ans_type.Add("Single Selected List Item 1");
+                                        q_selected_ans_type.Add("Single Selected List Item 2");
+                                        q_selected_ans_type.Add("Single Selected List Item 3");
+                                        q_selected_ans_type.Add("Single Selected List Item 4");
+                                        break;
+
+                                }//end of switch
+
+                                //QuestionHelpers.getDefault_Selected_Ans_list(x.obs_ans_type_category);
+                                ViewBag.q_selected_ans_type = q_selected_ans_type;
+                            }//end of else if(x.obs_ans_type_has_fxd_ans_yn == "Y")
+
+                        }//end of if (x.obs_ans_type_id == selected_ans_type_id)
+                        else
+                        {
+                            answer_type.Selected = false;
+                        }
+                        list_of_answers.Add(answer_type);
+                    }//end of foreach
+
+                    ViewBag.list_of_answers = list_of_answers;
+                    return View(list_of_answers);
+                }
+            } //end of   if (isSave == "false")         
+            ViewBag.list_of_answers = list_of_answers;
+            return View(list_of_answers);
         }
 
         // POST: Save default Answer Type.
@@ -191,11 +194,11 @@ namespace OBSMVC.Controllers
             bool isRequired = db.OBS_ANS_TYPE.Where(item => item.obs_ans_type_id == temp).Select(x => x.obs_ans_type_has_fxd_ans_yn).Equals("Y") ? true : false;
             return isRequired;
         }
-        /*public bool isNew_Quest_Ans_Type(int ans_type_id, int question_id)
+        public bool isNew_Quest_Ans_Type(int ans_type_id, int question_id)
         {
             short temp = (short)ans_type_id;
-           // bool isRequired = db.OBS_QUEST_ANS_TYPES.
-            return isRequired;
-        }*/
+            bool isNew_Quest_Ans_Type = false;
+            return isNew_Quest_Ans_Type;
+        }
     }
 }

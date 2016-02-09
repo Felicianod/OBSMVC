e90c772e9169581b0c1015bf7adad4c63ae17d9b
Rasul Abduguev <rasul.abduguev@dsc-logistics.com>
2016-02-09 16:56:45 -0600

    2/9 4:56pm DUPLICATES SUCK

--- OBSMVC/Controllers/TestQuestionAnswerTypeDefaultController.cs
+++ OBSMVC/Controllers/TestQuestionAnswerTypeDefaultController.cs
@@ -64,8 +64,8 @@ namespace OBSMVC.Controllers
             var all_answer_types = db.OBS_ANS_TYPE.Where(item => item.obs_ans_type_id > 0).ToList();
             string answer_type_id = formData["list_of_answers"];
             string question_id = formData["question_id"];
-            string isSave = formData["save"];
-            
+            string isSave = formData["save"];           
+
             string passed_selected_ans_types = formData["question_selected_ans_type"];
             if (isSave == "false")
             {
@@ -159,17 +159,21 @@ namespace OBSMVC.Controllers
                     ViewBag.list_of_answers = list_of_answers;
                     return View(list_of_answers);
                 }
-            } //end of   if (isSave == "false")         
+            } //end of   if (isSave == "false")
+            else
+            {//HERE, LETS SAVE THE DB CHANGES
+                SaveDefaultAnswerType(formData);
+            }        
             ViewBag.list_of_answers = list_of_answers;
             return View(list_of_answers);
         }
 
-        // POST: Save default Answer Type.
-        [HttpPost]
-        public ActionResult _SaveDefaultAnswerType(FormCollection formData)
+      
+        public void SaveDefaultAnswerType(FormCollection formData)
         {
             string answer_type_id = formData["list_of_answers"];
-            int question_id = Convert.ToInt32(formData["question_id"]);
+            int question_id = 58;         
+            //string default_sel_ans_types = formData["default_selected_ans_types"];
             //first lets check if user submitted None as a default answer type 
             if (String.IsNullOrEmpty(answer_type_id))
             {
@@ -182,7 +186,7 @@ namespace OBSMVC.Controllers
                 }
                 catch { }
             }//end  if (String.IsNullOrEmpty(answer_type_id))
-            else //if we're here, that means user submetted  answer type that <>None
+            else //if we're here, that means user submtted  answer type that <>None
             {
                 int selected_ans_type_id = Convert.ToInt32(formData["list_of_answers"]);
                 //now we need to check if this question/answer type combination already exist in obs_quest_ans_type table
@@ -192,25 +196,58 @@ namespace OBSMVC.Controllers
                     //first we need to check if this selected answer type requires a record in OBS_QUEST_SLCT_ANS
                     if (isQuest_Slct_Ans_Required(selected_ans_type_id)) {
                         //so we are here, that means we need to save both OBS_QUEST_ANS_TYPE and OBS_QUEST_SLCT_ANS
-                        OBS_QUEST_ANS_TYPES oBS_QUEST_ANS_TYPES = new OBS_QUEST_ANS_TYPES();
-                        oBS_QUEST_ANS_TYPES.obs_question_id = question_id;
-                        oBS_QUEST_ANS_TYPES.obs_ans_type_id = (short)selected_ans_type_id;
-                        oBS_QUEST_ANS_TYPES.obs_qat_default_ans_type_yn = "Y";
-                        oBS_QUEST_ANS_TYPES.obs_qat_end_eff_dt = Convert.ToDateTime("12/31/2060");
-                        db.OBS_QUEST_ANS_TYPES.Add(oBS_QUEST_ANS_TYPES);
-                        db.SaveChanges();
-                        
+
+                       //using (var transaction = db.Database.BeginTransaction())
+                        //{//need to create a transaction variable to rollback the changes for both tables if something goes wrong
+                            try {
+                                setExistingDefaultToN(question_id);
+                                string default_sel_ans_types = formData["default_selected_ans_types"];                              
+                                string[] splitterm = { "," };
+                                string[] selected_new_sel_ans_types = default_sel_ans_types.Split(splitterm, StringSplitOptions.RemoveEmptyEntries);                                                               
+                                OBS_QUEST_ANS_TYPES oBS_QUEST_ANS_TYPES = new OBS_QUEST_ANS_TYPES();
+                                oBS_QUEST_ANS_TYPES.obs_question_id = question_id;
+                                oBS_QUEST_ANS_TYPES.obs_ans_type_id = (short)selected_ans_type_id;
+                                oBS_QUEST_ANS_TYPES.obs_qat_default_ans_type_yn = "Y";
+                                oBS_QUEST_ANS_TYPES.obs_qat_end_eff_dt = Convert.ToDateTime("12/31/2060");
+                                db.OBS_QUEST_ANS_TYPES.Add(oBS_QUEST_ANS_TYPES);
+                                db.SaveChanges();
+                                short temp_selected_ans_type_id = (short)selected_ans_type_id;
+                                int qat_id = db.OBS_QUEST_ANS_TYPES.SingleOrDefault(item => item.obs_ans_type_id == temp_selected_ans_type_id && item.obs_question_id == question_id && item.obs_qat_end_eff_dt > DateTime.Today).obs_qat_id;
+                               
+                                short order = 1;
+                                foreach (string str in selected_new_sel_ans_types)
+                                {
+                                    OBS_QUEST_SLCT_ANS oBS_QUEST_SLCT_ANS = new OBS_QUEST_SLCT_ANS();
+                                    oBS_QUEST_SLCT_ANS.obs_qat_id = qat_id;
+                                    oBS_QUEST_SLCT_ANS.obs_qsa_text = str;
+                                    oBS_QUEST_SLCT_ANS.obs_qsa_order = order;
+                                    oBS_QUEST_SLCT_ANS.obs_qsa_dflt_yn = "N";
+                                    oBS_QUEST_SLCT_ANS.obs_qsa_eff_st_dt = DateTime.Today;
+                                    oBS_QUEST_SLCT_ANS.obs_qsa_eff_end_dt = Convert.ToDateTime("12/31/2060");
+                                    db.OBS_QUEST_SLCT_ANS.Add(oBS_QUEST_SLCT_ANS);
+                                   
+                                order++;
+                                }
+                               db.SaveChanges();
+                              }
+                            catch(Exception e)
+                            {
+                                string error = e.Message;
+                               // transaction.Rollback();
+                                //ViewBag.ResultMessage = "Error occured, records rolledback.";
+                            }
+                           
+                       // }//end of using (var transaction = db.Database.BeginTransaction())
 
                     }//end of if (isQuest_Slct_Ans_Required(selected_ans_type_id))
                     else {
-                        OBS_QUEST_ANS_TYPES oBS_QUEST_ANS_TYPES = new OBS_QUEST_ANS_TYPES();
-                        oBS_QUEST_ANS_TYPES.obs_question_id = question_id;
-                        oBS_QUEST_ANS_TYPES.obs_ans_type_id = (short)selected_ans_type_id;
-                        oBS_QUEST_ANS_TYPES.obs_qat_default_ans_type_yn = "Y";
-                        oBS_QUEST_ANS_TYPES.obs_qat_end_eff_dt = Convert.ToDateTime("12/31/2060");
-                        db.OBS_QUEST_ANS_TYPES.Add(oBS_QUEST_ANS_TYPES);
-                        db.SaveChanges();
-
+                        setExistingDefaultToN(question_id);
+                        OBS_QUEST_ANS_TYPES oBS_QUEST_ANS_TYPES_U = new OBS_QUEST_ANS_TYPES();
+                        oBS_QUEST_ANS_TYPES_U.obs_question_id = question_id;
+                        oBS_QUEST_ANS_TYPES_U.obs_ans_type_id = (short)selected_ans_type_id;
+                        oBS_QUEST_ANS_TYPES_U.obs_qat_default_ans_type_yn = "Y";
+                        oBS_QUEST_ANS_TYPES_U.obs_qat_end_eff_dt = Convert.ToDateTime("12/31/2060");
+                        save(oBS_QUEST_ANS_TYPES_U);
                     }
 
                 }//if (isNew_Quest_Ans_Type(selected_ans_type_id, question_id))
@@ -232,12 +269,12 @@ namespace OBSMVC.Controllers
                 }
                 
             }
-            return null;
+           
         }
         public bool isQuest_Slct_Ans_Required(int ans_type_id)
         {
             short temp = (short)ans_type_id;
-            bool isRequired = db.OBS_ANS_TYPE.Where(item => item.obs_ans_type_id == temp).Select(x => x.obs_ans_type_has_fxd_ans_yn).Equals("Y") ? true : false;
+            bool isRequired = (db.OBS_ANS_TYPE.Single(item => item.obs_ans_type_id == temp).obs_ans_type_has_fxd_ans_yn)=="Y" ? true : false;
             return isRequired;
         }
         /*
@@ -248,9 +285,68 @@ namespace OBSMVC.Controllers
         public bool isNew_Quest_Ans_Type(int ans_type_id, int question_id)
         {
             short temp = (short)ans_type_id;
-            int number_of_records = db.OBS_QUEST_ANS_TYPES.Where(item => item.obs_ans_type_id == temp && item.obs_question_id == question_id).ToList().Count;
-            bool isNew_Quest_Ans_Type = number_of_records==0?true:false;
-            return isNew_Quest_Ans_Type;
+            try
+            {
+                int number_of_records = db.OBS_QUEST_ANS_TYPES.Where(item => item.obs_ans_type_id == temp && item.obs_question_id == question_id).ToList().Count;
+                bool isNew_Quest_Ans_Type = number_of_records == 0 ? true : false;
+                return isNew_Quest_Ans_Type;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+        public void setExistingDefaultToN( int question_id)
+        {
+
+            OBS_QUEST_ANS_TYPES oBS_QUEST_ANS_TYPES = new OBS_QUEST_ANS_TYPES();
+            try
+            {
+                oBS_QUEST_ANS_TYPES = db.OBS_QUEST_ANS_TYPES.Single(item => item.obs_question_id == question_id && item.obs_qat_default_ans_type_yn == "Y");
+                oBS_QUEST_ANS_TYPES.obs_qat_default_ans_type_yn = "N";
+                db.SaveChanges();
+            }
+            catch { }
+        }
+        public void save(OBS_QUEST_ANS_TYPES oBS_QUEST_ANS_TYPES_U)
+        {
+            using (DSC_OBS_DB_ENTITY db1 = new DSC_OBS_DB_ENTITY())
+            {
+                db1.OBS_QUEST_ANS_TYPES.Add(oBS_QUEST_ANS_TYPES_U);
+                db1.SaveChanges();
+            }
+        }
+        public  List<String> getDefault_Selected_Ans_list(string answer_type_category)
+        {
+            List<string> q_selected_ans_type = new List<string>();
+            switch (answer_type_category)
+            {
+
+                case "3 Val Range":
+                    q_selected_ans_type.Add("Bad");
+                    q_selected_ans_type.Add("OK");
+                    q_selected_ans_type.Add("Good");
+                    break;
+                case "5 Val Range":
+                    q_selected_ans_type.Add("Monday");
+                    q_selected_ans_type.Add("Tuesday");
+                    q_selected_ans_type.Add("Wednesday");
+                    q_selected_ans_type.Add("Thursday");
+                    q_selected_ans_type.Add("Friday");
+                    break;
+                case "MS List":
+                    q_selected_ans_type.Add("MS List Item 1");
+                    q_selected_ans_type.Add("MS List Item 2");
+                    q_selected_ans_type.Add("MS List Item 3");
+                    q_selected_ans_type.Add("MS List Item 4");
+                    q_selected_ans_type.Add("MS List Item 5");
+                    break;
+                case "SS List":
+                    q_selected_ans_type.Add("Single Selected List Item");
+                    break;
+            }
+            return q_selected_ans_type;
         }
     }
 }

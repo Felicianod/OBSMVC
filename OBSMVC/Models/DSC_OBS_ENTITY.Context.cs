8648bbe01bdec458815f40de5f7a182464de52c6
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-02-23 16:31:40 -0600

    2/23 4:30

--- OBSMVC/Models/DSC_OBS_ENTITY.Context.cs
+++ OBSMVC/Models/DSC_OBS_ENTITY.Context.cs
@@ -32,7 +32,6 @@ namespace OBSMVC.Models
         public virtual DbSet<OBS_TYPE> OBS_TYPE { get; set; }
         public virtual DbSet<OBS_QUEST_ANS_TYPES> OBS_QUEST_ANS_TYPES { get; set; }
         public virtual DbSet<OBS_QUEST_ASSGND_MD> OBS_QUEST_ASSGND_MD { get; set; }
-        public virtual DbSet<OBS_QUESTION> OBS_QUESTION { get; set; }
         public virtual DbSet<OBS_QUESTION_METADATA> OBS_QUESTION_METADATA { get; set; }
         public virtual DbSet<OBS_ANS_TYPE> OBS_ANS_TYPE { get; set; }
         public virtual DbSet<DSC_CUSTOMER> DSC_CUSTOMER { get; set; }
@@ -55,5 +54,6 @@ namespace OBSMVC.Models
         public virtual DbSet<OBS_TYPE_SUB_TYPES> OBS_TYPE_SUB_TYPES { get; set; }
         public virtual DbSet<OBS_QUEST_SLCT_ANS> OBS_QUEST_SLCT_ANS { get; set; }
         public virtual DbSet<OBS_COL_FORM_QUESTIONS> OBS_COL_FORM_QUESTIONS { get; set; }
+        public virtual DbSet<OBS_QUESTION> OBS_QUESTION { get; set; }
     }
 }

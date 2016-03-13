63c9e3ed74cc759b1e8524e1d944ec24856f42ae
dsclogistics <feliciano.delgado@dsc-logistics.com>
2016-03-13 01:29:12 -0600

    .

---
+++ OBSMVC/Controllers/TestPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OBSMVC.Controllers
+{
+    public class TestPageController : Controller
+    {
+        // GET: TestPage
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}

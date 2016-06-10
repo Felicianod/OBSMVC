1e94eb243fa75b04f756dd4f23a9617096ca0c10
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-06-10 15:08:20 -0500

    Added Jump Page

--- OBSMVC/Controllers/HomeController.cs
+++ OBSMVC/Controllers/HomeController.cs
@@ -1,22 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
 using System.Web.Mvc;
 
 namespace OBSMVC.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
+            //Check whether this server needs to redirect the default "Index" entry point to the App Menu Selection Page or not            
+            try
+            {
+                if (ConfigurationManager.AppSettings["ServerType"].ToString().Equals("Development")) { return RedirectToAction("AppSelection", "Home"); }
+            }
+            catch
+            {
+            }
+
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
             //try
             //{
             //    if (!String.IsNullOrEmpty(Session["session_id"].ToString()))   { return View();}
             //}
             //catch{}
             //return RedirectToAction("OBSLogin", "Login");
+
+
+            
             return View();
         }
+
+        [AllowAnonymous]
+        public ActionResult AppSelection()
+        {
+            return View();
+        }
+
     }
+
+
+
+
 }
